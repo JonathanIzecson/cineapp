87b84e1dd5517a39648011ddc09a5ffa69d13826
JonathanIzecson <jonathanemmanuel.2292@gmail.com>
2021-05-23 17:44:15 -0500

    Integracion de la plantilla de bootstrap

--- config/Conexion.php
+++ config/Conexion.php
@@ -8,7 +8,6 @@ class Conexion{
     public function conectar(){
         try {
             $this->conexion = new PDO("mysql:host=localhost;dbname=cineapp","root","12345678");
-            echo "Conexion exitosa";
             return $this->conexion;
         } catch (PDOException $e) {
             echo $e->getMessage() . "</br>";
